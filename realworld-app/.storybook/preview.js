92402f26750d94f42cd71cba56f4a7c70015003e
Stella <stellax212@gmail.com>
2021-04-01 13:18:41 +0200

    Include Figma Designs

--- realworld-app/.storybook/preview.js
+++ realworld-app/.storybook/preview.js
@@ -1,6 +1,7 @@
 import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import {withDesign} from 'storybook-addon-designs';
 
 addDecorator(withInfo); 
 
