5d807cb3e64b316567490ddafa8039410754aac5
Stella <stellax212@gmail.com>
2021-04-02 11:09:50 +0200

    Follow button

--- realworld-app/src/app/ui/stories/follow_button_stories.cljs
+++ realworld-app/src/app/ui/stories/follow_button_stories.cljs
@@ -1,6 +1,9 @@
 (ns app.ui.stories.follow-button-stories
   (:require
    [app.ui.components.follow-button :as follow_button]
+   [helix.dom :as d]
+   ["react" :as react]
+   ["react-dom" :as rdom]
    [app.ui.lib :refer ($)]))
 
 
@@ -8,5 +11,11 @@
   #js {:title "Follow Button"
        :component follow_button/FollowButton})
 
-(defn ^:export Default []
+(defn ^:export Online []
   ($ follow_button/FollowButton))
+
+
+(defn ^:export Design [text]
+  (d/button
+   {:class ["btn btn-sm btn-secondary"]}
+   (d/i {:class "ion-plus-round"}) " Follow"))
